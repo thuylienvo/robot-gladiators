81b709da88a86f5f9362a14f50df52797e369b4d
Mimi Vo <mimzy414@gmail.com>
2021-03-31 15:13:05 -0700

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -12,6 +12,25 @@ console.log(enemyNames.length);
 console.log(enemyNames[0]);
 console.log(enemyNames[3]);
 
+// function to start a new game
+var startGame = function() {
+    for (var i = 0; i < enemyNames.length; i++) {
+      if (playerHealth > 0) {
+        window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+  
+        var pickedEnemyName = enemyNames[i];
+  
+        enemyHealth = 50;
+  
+        fight(pickedEnemyName);
+      }
+      else {
+        window.alert("You have lost your robot in battle! Game Over!");
+        break;
+      }
+    }
+  };
+
 // fight function (now with parameter for enemy's name)
 var fight = function(enemyName) {
   while (playerHealth > 0 && enemyHealth > 0) {
@@ -94,3 +113,12 @@ for (var i = 0; i < enemyNames.length; i++) {
     break;
   }
 }
+// start the game when the page loads
+var startGame = function() {
+    for (var i = 0; i < enemyNames.length; i++) {
+      ...
+    }
+  
+    // play again
+    startGame();
+  };
